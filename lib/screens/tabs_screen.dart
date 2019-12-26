18e83c3ba93f3ffe0767cf9bf225778b9b2f318a
Mike Guijarro <mguijarro39@gmail.com>
2019-12-26 07:34:43 -0600

    Implementa bottom navigation bar con statefull widget

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -3,7 +3,6 @@ import './favorites_screen.dart';
 import './categories_screen.dart';
 
 class TabsScreen extends StatefulWidget {
-
   //static const String routeName = "/tab";
 
   @override
@@ -11,40 +10,46 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {"page": CategoriesScreen(), "title": "Categories"},
+    {"page": FavoritesScreen(), "title": "Favorite"},
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectedPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("DUMMY HOLA"),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: "Categories",
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: "Favorite",
-              )
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]["title"]),
+      ),
+      body: _pages[_selectedPageIndex]["page"],
+      bottomNavigationBar: BottomNavigationBar(
+        //Todo se maneja manualmente
+        onTap: _selectedPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            title: Text("Categories"),
           ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            /* 
-              Nota Cool:
-              Hay que evitar que las pantallas que se carguen al TabBarView
-              contengan Scaffolds widgets. De esta forma evitamos tener un appbar
-              duplicado.
-              El scaffold que tenemos en tabs_screen se hereda naturalmente a los 
-              widgets que se carguen en la configuracion de tabs.
-            */
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            title: Text("Favorites"),
+          ),
+        ],
       ),
     );
   }
