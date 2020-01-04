2490cf9c70a27ee3b9921f9a5faa23bcdb299bdb
Mike Guijarro <mguijarro39@gmail.com>
2020-01-03 21:21:14 -0600

    Agrega seccion de filtros de forma dummy, se pasa el estado entre screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import './screens/filters_screen.dart';
 
+import './dummy-data.dart';
+import './models/meal.dart';
+import './screens/filters_screen.dart';
 import './screens/tabs_screen.dart';
 import './screens/meal_detail_screen.dart';
 import './screens/category_meals_screen.dart';
@@ -8,8 +10,43 @@ import './screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    "gluten": false,
+    "lactose": false,
+    "vegan": false,
+    "vegetarian": false,
+  };
+
+  List<Meal> _availableMeals = DUMMY_MEALS;
+
+  _setFilters(Map<String, bool> filterData){
+    setState(() {
+      _filters = filterData;
+
+      _availableMeals = DUMMY_MEALS.where((meal){
+        if (_filters["gluten"] && !meal.isGlutenFree) {
+          return false;
+        }
+        if (_filters["lactose"] && !meal.isLactoseFree) {
+          return false;
+        }
+        if (_filters["vegan"] && !meal.isVegan) {
+          return false;
+        }
+        if (_filters["vegetarian"] && !meal.isVegetarian) {
+          return false;
+        }
+        return true;
+      }).toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,9 +56,9 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.amber,
           canvasColor: Color.fromRGBO(255, 254, 229, 1),
           fontFamily: "Raleway",
-          textTheme: ThemeData.light().textTheme.copyWith( 
-            //Copy with permite suplir valores que se tienen por default
-            //con otros que se sobreescriben.
+          textTheme: ThemeData.light().textTheme.copyWith(
+                //Copy with permite suplir valores que se tienen por default
+                //con otros que se sobreescriben.
                 body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
                 body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
                 title: TextStyle(
@@ -33,25 +70,24 @@ class MyApp extends StatelessWidget {
       initialRoute: "/",
       routes: {
         // "/": (ctx)=> CategorieScreens() nos permite entrar como una ruta por default.
-        "/": (ctx)=> TabsScreen(),
-        CategoryMealsScreen.routeName: (ctx)=> CategoryMealsScreen(),
-        MealDetailScreen.routeName: (ctx)=> MealDetailScreen(),
-        FiltersScreen.routeName: (ctx)=> FiltersScreen(),
-
+        "/": (ctx) => TabsScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(_availableMeals),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(_filters, _setFilters),
       },
       /* En caso de que nuestra navegacion estandar llegue a fallar,
           esta configuracion nos permite entrar a una ruta por default
           */
-      onGenerateRoute: (routeSettings) {  
+      onGenerateRoute: (routeSettings) {
         print(routeSettings.arguments);
-        return MaterialPageRoute(builder: (buildCtx)=> CategoriesScreen());
+        return MaterialPageRoute(builder: (buildCtx) => CategoriesScreen());
       },
       /* Este metodo es el ultimo recurso que usa flutter para revisar las 
       rutas que tiene disponibles antes de que ejecute un error.
       Este metodo se activa en caso de que no se tenga ninguna ruta disponible.
       El equivalente en web de la pagina 404 no existe.*/
       onUnknownRoute: (routeSettings) {
-        return MaterialPageRoute(builder: (buildCtx)=> CategoriesScreen());
+        return MaterialPageRoute(builder: (buildCtx) => CategoriesScreen());
       },
     );
   }
